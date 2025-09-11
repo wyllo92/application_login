7a601534f64c023f34e250cc78c5c6a7423ca45e
wyllo92 <williamf_arias@soy.sena.edu.co>
2025-09-10 22:46:22 -0500

    flutter login

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'pages/splash/splash_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'flutter demo app',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const SplashScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/pages/splash/splash_app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import '..auth/login.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
